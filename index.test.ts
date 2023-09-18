3afb16b018c70cf41cc729a24a34ab7e3b61a4f9
Mani Malarvannan <manivannan.malarvannan@xcelenergy.com>
2023-09-18 14:13:15 -0500

    first commit

---
+++ index.test.ts
@@ -0,0 +1,132 @@
+import { City, Reader } from '@maxmind/geoip2-node'
+import { Plugin, PluginMeta } from '@analytickit/plugin-scaffold'
+// @ts-ignore
+import { createPageview, resetMeta } from '@analytickit/plugin-scaffold/test/utils'
+import { join } from 'path'
+
+import * as index from '.'
+
+const { processEvent } = index as Required<Plugin>
+
+const DEFAULT_MMDB_FILE_NAME = 'GeoLite2-City-Test.mmdb'
+
+async function resetMetaWithMmdb(
+    transformResult = (res: City) => res as Record<string, any>,
+    file = DEFAULT_MMDB_FILE_NAME
+): Promise<PluginMeta> {
+    const mmdb = await Reader.open(join(__dirname, file))
+    return resetMeta({
+        geoip: {
+            locate: (ipAddress: string) => {
+                const res = mmdb.city(ipAddress)
+                return transformResult(res)
+            },
+        },
+    }) as PluginMeta
+}
+
+test('event is enriched with IP location', async () => {
+    const event = await processEvent({ ...createPageview(), ip: '89.160.20.129' }, await resetMetaWithMmdb())
+    expect(event!.properties).toEqual(
+        expect.objectContaining({
+            $geoip_city_name: 'Linköping',
+            $geoip_country_name: 'Sweden',
+            $geoip_country_code: 'SE',
+            $geoip_continent_name: 'Europe',
+            $geoip_continent_code: 'EU',
+            $geoip_latitude: 58.4167,
+            $geoip_longitude: 15.6167,
+            $geoip_time_zone: 'Europe/Stockholm',
+            $geoip_subdivision_1_code: 'E',
+            $geoip_subdivision_1_name: 'Östergötland County',
+        })
+    )
+})
+
+test('person is enriched with IP location', async () => {
+    const event = await processEvent({ ...createPageview(), ip: '89.160.20.129' }, await resetMetaWithMmdb())
+    expect(event!.properties!.$set).toEqual(
+        expect.objectContaining({
+            $geoip_city_name: 'Linköping',
+            $geoip_country_name: 'Sweden',
+            $geoip_country_code: 'SE',
+            $geoip_continent_name: 'Europe',
+            $geoip_continent_code: 'EU',
+            $geoip_latitude: 58.4167,
+            $geoip_longitude: 15.6167,
+            $geoip_time_zone: 'Europe/Stockholm',
+            $geoip_subdivision_1_code: 'E',
+            $geoip_subdivision_1_name: 'Östergötland County',
+        })
+    )
+    expect(event!.properties!.$set_once).toEqual(
+        expect.objectContaining({
+            $initial_geoip_city_name: 'Linköping',
+            $initial_geoip_country_name: 'Sweden',
+            $initial_geoip_country_code: 'SE',
+            $initial_geoip_continent_name: 'Europe',
+            $initial_geoip_continent_code: 'EU',
+            $initial_geoip_latitude: 58.4167,
+            $initial_geoip_longitude: 15.6167,
+            $initial_geoip_time_zone: 'Europe/Stockholm',
+            $initial_geoip_subdivision_1_code: 'E',
+            $initial_geoip_subdivision_1_name: 'Östergötland County',
+        })
+    )
+})
+
+test('person props default to null if no values present', async () => {
+    const removeCityNameFromLookupResult = (res: City) => {
+        const { city, ...remainingResult } = res
+        return remainingResult
+    }
+    const event = await processEvent(
+        { ...createPageview(), ip: '89.160.20.129' },
+        await resetMetaWithMmdb(removeCityNameFromLookupResult)
+    )
+    expect(event!.properties!.$set).toEqual({
+        $geoip_city_name: null, // default to null
+        $geoip_country_name: 'Sweden',
+        $geoip_country_code: 'SE',
+        $geoip_continent_name: 'Europe',
+        $geoip_continent_code: 'EU',
+        $geoip_latitude: 58.4167,
+        $geoip_longitude: 15.6167,
+        $geoip_time_zone: 'Europe/Stockholm',
+        $geoip_subdivision_1_code: 'E',
+        $geoip_subdivision_1_name: 'Östergötland County',
+        $geoip_subdivision_2_code: null,
+        $geoip_subdivision_2_name: null,
+        $geoip_postal_code: null,
+    })
+    expect(event!.properties!.$set_once).toEqual({
+        $initial_geoip_city_name: null, // default to null
+        $initial_geoip_country_name: 'Sweden',
+        $initial_geoip_country_code: 'SE',
+        $initial_geoip_continent_name: 'Europe',
+        $initial_geoip_continent_code: 'EU',
+        $initial_geoip_latitude: 58.4167,
+        $initial_geoip_longitude: 15.6167,
+        $initial_geoip_time_zone: 'Europe/Stockholm',
+        $initial_geoip_subdivision_1_code: 'E',
+        $initial_geoip_subdivision_1_name: 'Östergötland County',
+        $initial_geoip_subdivision_2_code: null,
+        $initial_geoip_subdivision_2_name: null,
+        $initial_geoip_postal_code: null,
+    })
+})
+
+test('error is thrown if meta.geoip is not provided', async () => {
+    expect.assertions(1)
+    await expect(
+        async () => await processEvent({ ...createPageview(), ip: '89.160.20.129' }, resetMeta())
+    ).rejects.toEqual(
+        new Error('This AnalyticKit version does not have GeoIP capabilities! Upgrade to AnalyticKit 1.24.0 or later')
+    )
+})
+
+test('event is skipped using $geoip_disable', async () => {
+    const testEvent = { ...createPageview(), ip: '89.160.20.129', properties: { $geoip_disable: true } }
+    const processedEvent = await processEvent(JSON.parse(JSON.stringify(testEvent)), await resetMetaWithMmdb())
+    expect(testEvent).toEqual(processedEvent)
+})
---
+++ index.ts
@@ -0,0 +1,104 @@
+import { Plugin } from '@analytickit/plugin-scaffold'
+
+const ONE_DAY = 60 * 60 * 24 // 24h in seconds
+
+const defaultLocationSetProps = {
+    $geoip_city_name: null,
+    $geoip_subdivision_2_name: null,
+    $geoip_subdivision_2_code: null,
+    $geoip_subdivision_1_name: null,
+    $geoip_subdivision_1_code: null,
+    $geoip_country_name: null,
+    $geoip_country_code: null,
+    $geoip_continent_name: null,
+    $geoip_continent_code: null,
+    $geoip_postal_code: null,
+    $geoip_latitude: null,
+    $geoip_longitude: null,
+    $geoip_time_zone: null,
+}
+
+const defaultLocationSetOnceProps = {
+    $initial_geoip_city_name: null,
+    $initial_geoip_subdivision_2_name: null,
+    $initial_geoip_subdivision_2_code: null,
+    $initial_geoip_subdivision_1_name: null,
+    $initial_geoip_subdivision_1_code: null,
+    $initial_geoip_country_name: null,
+    $initial_geoip_country_code: null,
+    $initial_geoip_continent_name: null,
+    $initial_geoip_continent_code: null,
+    $initial_geoip_postal_code: null,
+    $initial_geoip_latitude: null,
+    $initial_geoip_longitude: null,
+    $initial_geoip_time_zone: null,
+}
+
+const plugin: Plugin = {
+    processEvent: async (event, { geoip }) => {
+        if (!geoip) {
+            throw new Error('This AnalyticKit version does not have GeoIP capabilities! Upgrade to AnalyticKit 1.24.0 or later')
+        }
+        let ip = event.properties?.$ip || event.ip
+        if (ip && !event.properties?.$geoip_disable) {
+            ip = String(ip)
+            if (ip === '127.0.0.1') {
+                ip = '13.106.122.3' // Spoofing an Australian IP address for local development
+            }
+            const response = await geoip.locate(ip)
+            if (response) {
+                const location: Record<string, any> = {}
+                if (response.city) {
+                    location['city_name'] = response.city.names?.en
+                }
+                if (response.country) {
+                    location['country_name'] = response.country.names?.en
+                    location['country_code'] = response.country.isoCode
+                }
+                if (response.continent) {
+                    location['continent_name'] = response.continent.names?.en
+                    location['continent_code'] = response.continent.code
+                }
+                if (response.postal) {
+                    location['postal_code'] = response.postal.code
+                }
+                if (response.location) {
+                    location['latitude'] = response.location?.latitude
+                    location['longitude'] = response.location?.longitude
+                    location['time_zone'] = response.location?.timeZone
+                }
+                if (response.subdivisions) {
+                    for (const [index, subdivision] of response.subdivisions.entries()) {
+                        location[`subdivision_${index + 1}_code`] = subdivision.isoCode
+                        location[`subdivision_${index + 1}_name`] = subdivision.names?.en
+                    }
+                }
+
+                if (!event.properties) {
+                    event.properties = {}
+                }
+
+                if (!event.properties.$set) {
+                    event.properties.$set = {}
+                }
+                if (!event.properties.$set_once) {
+                    event.properties.$set_once = {}
+                }
+                event.properties.$set = { ...defaultLocationSetProps, ...(event.properties.$set ?? {}) }
+                event.properties.$set_once = {
+                    ...defaultLocationSetOnceProps,
+                    ...(event.properties.$set_once ?? {}),
+                }
+
+                for (const [key, value] of Object.entries(location)) {
+                    event.properties[`$geoip_${key}`] = value
+                    event.properties.$set![`$geoip_${key}`] = value
+                    event.properties.$set_once![`$initial_geoip_${key}`] = value
+                }
+            }
+        }
+        return event
+    },
+}
+
+module.exports = plugin
